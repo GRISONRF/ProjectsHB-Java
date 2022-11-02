8ea4be8099e9bdb60150171345901bfc4b0b42b5
GRISONRF <grisonrf@gmail.com>
2022-11-02 16:14:22 -0600

    add user greetings

---
+++ Unit1/src/GuessingGame.java
@@ -0,0 +1,18 @@
+public class GuessingGame {
+    /*
+greet player
+get player name
+choose random number between 1 and 100
+repeat forever:
+   get guess
+   if guess is incorrect:
+      give hint
+      increase number of guesses
+   else:
+      congratulate player  */
+    public static void main(String[] args) {
+
+        System.out.println("Hello player!");
+    }
+
+}
