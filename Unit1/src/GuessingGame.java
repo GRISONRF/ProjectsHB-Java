125d767a8f66cc767ead7a7ad6eb7cb35a9c49d4
GRISONRF <grisonrf@gmail.com>
2022-11-02 17:01:56 -0600

    game is ready

--- Unit1/src/GuessingGame.java
+++ Unit1/src/GuessingGame.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
     /*
@@ -14,11 +15,38 @@ repeat forever:
       congratulate player  */
     public static void main(String[] args) {
 
-        System.out.println("Hello player!");
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the player's name? ");
+        System.out.println("Hello player! \n What is your name? ");
         String player = scan.nextLine();
-        System.out.println(player);
+
+        Random rand = new Random();
+        //call method nextInt and pass in upper range bound. https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/util/Random.html
+        int number = rand.nextInt(100);
+        System.out.println(number);
+        System.out.println(player + " pick a number between 0 - 100: ");
+        int guess = scan.nextInt();
+        int tries = 0;
+
+
+        while (true) {
+            tries = tries + 1;
+            if (guess > number) {
+                System.out.println("Your guess is too high, pick a new number: ");
+                guess = scan.nextInt();
+            } else if (guess < number){
+                System.out.println("This number is too low, pick a new number: ");
+                guess = scan.nextInt();
+            } else {
+                System.out.println("Congratulations, you guessed right!");
+                System.out.println("It took you " + tries + " tries.");
+                break;
+            }
+        }
+
+
+
+
     }
 
 }
