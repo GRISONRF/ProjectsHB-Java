8d3e802f2394c0dee19c8373df03d67f659e0a17
GRISONRF <grisonrf@gmail.com>
2022-11-04 19:56:43 -0600

    mars expedition is done

--- Unit1/src/MarsExpedition.java
+++ Unit1/src/MarsExpedition.java
@@ -10,14 +10,14 @@ public class MarsExpedition {
         String name = input.nextLine();
         System.out.println("Hello " + name + ", welcome to the Expedition prep program. Are you ready to head out into the new world? Type Y or N");
         String yN = input.nextLine();
-        if (yN.equals("y")) {
+        if (yN.equalsIgnoreCase("y")) {
             System.out.println("I knew you would say that. You are team leader for a reason.");
-        } else if (yN.equals("n")) {
+        } else if (yN.equalsIgnoreCase("n")) {
             System.out.println("Too bad you are team leader. You have to go.");
         }
         System.out.println("How many people do you want in your team?");
         int team = input.nextInt();
-        if (team > 2){
+        if (team > 2) {
             System.out.println("That’s way to many people. We can only send 2 more members.");
             team = 2;
         } else if (team < 2) {
@@ -30,6 +30,33 @@ public class MarsExpedition {
         System.out.println("You are allowed to bring one snack with you. What do you want to bring?");
         String snack = input.nextLine();
         System.out.println("Nice choice, you will be bringing a " + " with you.");
+
+        System.out.println("You have the choice of 3 vehicles" +
+                "\nA: A Mars Rover" +
+                "\nB: A Chevy Silverado" +
+                "\nC: A Honda Civic \n Pick one. ");
+        String choice = input.nextLine();
+
+        if (choice.equalsIgnoreCase("A")) {
+            choice = " a Mars Rover";
+        } else if (choice.equalsIgnoreCase("B")) {
+            choice = " a Chevy Silverado";
+        } else if (choice.equalsIgnoreCase("C")) {
+            choice = " a Honda Civic";
+        } else {
+            choice = "your feet";
+        }
+
+        System.out.println("Hello " + name + "!" +
+                "\n Your team have a size of " + team + ", you are bringing " + snack + " with you, and you are going with " + choice + "." +
+                "\n5...." +
+                "\n4...." +
+                "\n3...." +
+                "\n2...." +
+                "\n1....");
+
+
+
 
     }
 }
